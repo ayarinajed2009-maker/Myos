9362eee6d643248a6816ad54d0ad2a83524b8aef
ayarinajed2009-maker <ayarinajed2009@gmail.com>
2026-01-11 06:08:38 -0700

    Create kernel.c

---
+++ kernel.c
@@ -0,0 +1,19 @@
+// kernel.c
+#define VIDEO_MEMORY 0xB8000
+#define WHITE_ON_BLACK 0x0F
+
+void print(const char* str) {
+    char* video = (char*) VIDEO_MEMORY;
+    int i = 0;
+
+    while (str[i]) {
+        video[i * 2] = str[i];
+        video[i * 2 + 1] = WHITE_ON_BLACK;
+        i++;
+    }
+}
+
+void kernel_main() {
+    print("My C Operating System");
+    while (1);
+}
